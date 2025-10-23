ab6c908a726b2a410b5a1a0b0905353a88711f98
ZeeshanZk09 <mzeeshankhan0988@gmail.com>
2025-10-24 04:54:53 +0500

    functions added

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,49 @@
+import Prisma from '@/lib/prisma';
 import { inngest } from './client';
 
-export const helloWorld = inngest.createFunction(
-  { id: 'hello-world' },
-  { event: 'test/hello.world' },
-  async ({ event, step }) => {
-    await step.sleep('wait-a-moment', '1s');
-    return { message: `Hello ${event.data.email}!` };
+export const syncUserCreation = inngest.createFunction(
+  { id: 'sync-user-creation' },
+  { event: 'clerk/clerk.created' },
+  async ({ event }) => {
+    const { data } = event;
+    await Prisma.user.create({
+      data: {
+        id: data?.id,
+        name: `${data?.first_name} ${data?.last_name}`,
+        email: data?.email_addresses[0]?.email_address,
+        image: data?.image_url,
+      },
+    });
+  }
+);
+
+export const syncUserUpdation = inngest.createFunction(
+  { id: 'sync-user-updation' },
+  { event: 'clerk/clerk.updated' },
+  async ({ event }) => {
+    const { data } = event;
+    await Prisma.user.update({
+      where: {
+        id: data?.id,
+      },
+      data: {
+        name: `${data?.first_name} ${data?.last_name}`,
+        email: data?.email_addresses[0]?.email_address,
+        image: data?.image_url,
+      },
+    });
+  }
+);
+
+export const syncUserDeletion = inngest.createFunction(
+  { id: 'sync-user-deletion' },
+  { event: 'clerk/clerk.deleted' },
+  async ({ event }) => {
+    const { data } = event;
+    await Prisma.user.delete({
+      where: {
+        id: data?.id,
+      },
+    });
   }
 );
